66240e2c34129f7581a87d96c37739368e27525f
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-04-25 03:48:08 +0600

    feat: add custom query method findByFirstName to StudentRepository
    - Defined findByFirstName derived query method.
    - Enabled automated SQL generation for filtering students by first name.
    - Followed Spring Data JPA naming conventions for query derivation.

--- src/main/java/com/chronicle/spring_data_jpa/jpa_demo/repository/StudentRepository.java
+++ src/main/java/com/chronicle/spring_data_jpa/jpa_demo/repository/StudentRepository.java
@@ -4,7 +4,11 @@ import com.chronicle.spring_data_jpa.jpa_demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    
+    public List<Student> findByFirstName(String firstName);
+    
 }
