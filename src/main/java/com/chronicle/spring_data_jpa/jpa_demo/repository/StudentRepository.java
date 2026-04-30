ee966f299e5227e0c96eefd856f8b097abaeac73
Fatema Tuj Zohra <fatematujzohra20@gmail.com>
2026-04-30 17:34:57 +0600

    feat: implement JPQL query with indexed parameters
    - Added findByFirstNameAndLastName using the @Query annotation.
    - Utilized indexed parameters (?1, ?2) for positional argument binding.
    - Defined the method to return a single Student object instead of a List.

--- src/main/java/com/chronicle/spring_data_jpa/jpa_demo/repository/StudentRepository.java
+++ src/main/java/com/chronicle/spring_data_jpa/jpa_demo/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.chronicle.spring_data_jpa.jpa_demo.repository;
 
 import com.chronicle.spring_data_jpa.jpa_demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,11 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByLastNameNotNull();
     
     List<Student> findByGuardianName(String guardianName);
+    
+    // JPQL
+    @Query("select s from Student s where s.firstName = ?1 and s.lastName = ?2")
+    Student findByFirstNameAndLastName(String firstName, String lastName);
+    
+
     
 }
